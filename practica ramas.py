9087e93b99ce20e0b4f0475129fc00187572be9d
guadalupe22-mzm <mariamorazuniga22@gmail.com>
2024-11-01 21:56:47 -0600

    ejercicio ramas

---
+++ practica ramas.py
@@ -0,0 +1,11 @@
+num1 = int(input("digite su numero:"))
+resta = num1 - 100
+
+if num1 > 100:
+    print ("el valor es: ", resta)
+    
+if num1 <= 100 and num1 >0: 
+    print ("el valor del numero es: ", num1) 
+    
+else:  
+    print("no se pueden numeron menores a 0")
